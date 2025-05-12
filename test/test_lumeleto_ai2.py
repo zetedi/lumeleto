496056dfc7da705d9e8e080c3989601363558282
Zoltán Etédi <zetedi.external@epo.org>
2025-05-12 21:16:37 +0100

    ai2

---
+++ test/test_lumeleto_ai2.py
@@ -0,0 +1,32 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+model_dir = "/Volumes/lumeleto//ai2-lumeleto"
+tokenizer = AutoTokenizer.from_pretrained(model_dir)
+model = AutoModelForCausalLM.from_pretrained(model_dir)
+
+model.eval()
+
+# Run on CPU
+inputs = tokenizer("What is the Lifetree Network good for?", return_tensors="pt")
+# outputs = model.generate(
+#    **inputs,
+#    max_new_tokens=50,
+#    do_sample=True,
+#    top_k=40,
+#    top_p=0.95,
+#    temperature=0.7,
+#    pad_token_id=tokenizer.eos_token_id,
+#)
+
+
+outputs = model.generate(
+    **inputs,
+    max_new_tokens=80,
+    pad_token_id=tokenizer.eos_token_id,
+    do_sample=True,            # Enable sampling
+    temperature=0.8,           # Add randomness
+    top_p=0.9,                 # Nucleus sampling
+)
+
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
